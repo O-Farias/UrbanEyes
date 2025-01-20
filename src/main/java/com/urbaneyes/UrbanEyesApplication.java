108aefbeda2344519f55002a67d90effa8dc9f19
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-20 23:10:17 +0000

    feat: cria classe principal UrbanEyesApplication

---
+++ src/main/java/com/urbaneyes/UrbanEyesApplication.java
@@ -0,0 +1,12 @@
+package com.urbaneyes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UrbanEyesApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UrbanEyesApplication.class, args);
+    }
+}
