2e4bc10a29b8facfcec0ad17988a9a1ed90222bf
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-20 23:59:52 +0000

    refactor: ajusta estrutura do IssueRepository - Melhorias no arquivo `IssueRepository.java` para manter consistência no projeto.

--- src/main/java/com/urbaneyes/repositories/IssueRepository.java
+++ src/main/java/com/urbaneyes/repositories/IssueRepository.java
@@ -2,6 +2,8 @@ package com.urbaneyes.repository;
 
 import com.urbaneyes.model.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
 }
