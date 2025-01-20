0a318932d53beb12d2f2dfef8f28581be213cd85
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-20 23:48:43 +0000

    feat: adiciona UserController para gerenciamento de usuários

---
+++ src/main/java/com/urbaneyes/controllers/UserController.java
@@ -0,0 +1,33 @@
+package com.urbaneyes.controller;
+
+import com.urbaneyes.model.User;
+import com.urbaneyes.service.UserService;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+
+    private final UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @PostMapping("/register")
+    public User registerUser(@RequestBody User user) {
+        return userService.registerUser(user);
+    }
+
+    @PostMapping("/login")
+    public String loginUser(@RequestBody User user) {
+        return userService.loginUser(user);
+    }
+
+    @GetMapping
+    public List<User> getAllUsers() {
+        return userService.getAllUsers();
+    }
+}
