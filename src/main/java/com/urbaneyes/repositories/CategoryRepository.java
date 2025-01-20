8ee984a7b9e0231acd620bd7e9dcd50aa077b92b
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-20 23:59:30 +0000

    feat: cria repository para categorias - Arquivo `CategoryRepository.java` adicionado para gerenciamento de persistência da entidade `Category`.

---
+++ src/main/java/com/urbaneyes/repositories/CategoryRepository.java
@@ -0,0 +1,9 @@
+package com.urbaneyes.repository;
+
+import com.urbaneyes.model.Category;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+}
