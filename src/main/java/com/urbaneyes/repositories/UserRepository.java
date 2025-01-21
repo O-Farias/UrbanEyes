a902a045af309a38c2df15f68acae3153921cafd
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-21 00:00:11 +0000

    feat: cria repository para usuários -Arquivo `UserRepository.java` adicionado para gerenciamento de persistência da entidade `User`.

---
+++ src/main/java/com/urbaneyes/repositories/UserRepository.java
@@ -0,0 +1,9 @@
+package com.urbaneyes.repository;
+
+import com.urbaneyes.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+}
