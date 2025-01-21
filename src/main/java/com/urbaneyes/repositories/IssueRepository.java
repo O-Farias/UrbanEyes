6d8467ccd660088bbbb2b212ad125a238dbdfa63
Mateus Farias <mateus.f2001@hotmail.com>
2025-01-21 00:25:47 +0000

    fix: ajusta IssueRepository para corrigir inconsistências no mapeamento

--- src/main/java/com/urbaneyes/repositories/IssueRepository.java
+++ src/main/java/com/urbaneyes/repositories/IssueRepository.java
@@ -4,6 +4,9 @@ import com.urbaneyes.model.Issue;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface IssueRepository extends JpaRepository<Issue, Long> {
+    List<Issue> findByCategoryId(Long categoryId); 
 }
